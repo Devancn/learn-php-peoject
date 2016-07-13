fa85bd0e307fc1294f8f835b39e54af794de2656
dingxiaohuan <wydingxiaohuan@163.com>
2016-07-14 00:20:44 +0800

    删除商品相册

--- php38/Application/Runtime/Cache/Admin/608b0c961c661bd6d45820311b4b75a7.php
+++ php38/Application/Runtime/Cache/Admin/608b0c961c661bd6d45820311b4b75a7.php
@@ -34,7 +34,7 @@
         </p>
     </div>
     <div id="tabbody-div">
-        <form enctype="multipart/form-data" action="/index.php/Admin/Goods/edit/id/42/p/2.html" method="post">
+        <form enctype="multipart/form-data" action="/index.php/Admin/Goods/edit/id/43/p/1.html" method="post">
             <input type="hidden" name="id" value="<?php echo I('get.id'); ?>" />
             <!-- 基本信息 -->
             <table width="90%" class="table_form" id="general-table" align="center">
